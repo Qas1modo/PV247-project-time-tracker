695e93f71a6420aa33a6600f81215efe5cb01045
Radomír Mann <493346@mail.muni.cz>
2023-11-26 01:42:41 +0100

    Database queries

---
+++ src/server/Record/record.ts
@@ -0,0 +1,102 @@
+import { db } from '@/server/db';
+import { getActivityById } from '../Activity/activity';
+
+export const getRecordById = async (data: { id: number }) => {
+    const record = await db.record.findFirst({ where: { id: data.id, 
+        deleted: false, activity: {
+            deleted: false
+        } }, select: {
+            activity: {
+                include: {
+                    category: {
+                        select: {
+                            name: true
+                        }
+                    }
+                }
+            },
+        }});
+    return record;
+};
+
+export const getUserRecords = async (data: { userId: number }) => {
+    const records = await db.activity.findMany({where: {
+        userId: data.userId, deleted: false
+    }, select: {
+        name: true,
+        records: {
+            where: {
+                deleted: false
+            }
+        }
+    }});
+    return records;
+};
+
+export const getUserDoneRecords = async (data: { userId: number }) => {
+    const records = await db.activity.findMany({where: {
+        userId: data.userId,
+        deleted: false
+    }, select: {
+        name: true,
+        records: {
+            where: {NOT: [
+                {
+                    endedAt: null
+                }
+            ],
+            deleted: false
+        }
+    }
+    }});
+    return records;
+};
+
+export const getUserProgressRecords = async (data: { userId: number }) => {
+    const records = await db.activity.findMany({where: {
+        userId: data.userId,
+    }, select: {
+        name: true,
+        records: {
+            where: {
+                endedAt: null,
+                deleted: false
+            }
+        }
+    }});
+    return records;
+}
+
+export const startRecord = async (data: { activityId: number, userId: number }) => {
+    if ((await getActivityById({id: data.activityId}))?.userId != data.userId) {
+        return;
+    }
+    const record = db.record.create({ data: {
+        activityId: data.activityId
+    }});
+    return record;
+}
+
+export const finishRecord = async (data: { userId: number, recordId: number}) => {
+    const record = db.record.update({where: {
+        id: data.recordId,
+        activity: {
+            userId: data.userId
+        }
+    }, data: {
+        endedAt: Date()
+    }});
+    return record;
+}
+
+export const deleteRecord = async (data: { userId: number, recordId: number}) => {
+    const record = db.record.update({where: {
+        id: data.recordId,
+        activity: {
+            userId: data.userId
+        }
+    }, data: {
+        deleted: true
+    }});
+    return record;
+}
---
+++ src/server/User/user.ts
@@ -0,0 +1,13 @@
+import { db } from '@/server/db';
+
+export const getUser = async (data: { id: number }) => {
+    const user = await db.user.findFirst({where: { id: data.id }});
+    return Response.json(user);
+  };
+
+export const getUsers = async () => {
+    const users = await db.user.findMany();
+    return Response.json(users);
+};
+
+
