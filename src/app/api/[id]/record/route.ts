6d38e97c00a213d865d70d7598fc88631fe6d684
Radomír Mann <493346@mail.muni.cz>
2023-11-26 02:15:18 +0100

    Buildable

---
+++ src/app/api/[id]/record/route.ts
@@ -0,0 +1,16 @@
+import { getRecordById } from '@/server/Record/record';
+
+export const GET = async (
+	req: Request,
+	{ params }: { params: { id: number } }
+) => {
+	const data = await getRecordById({ id: params.id });
+    if (data === null) {
+        return new Response(JSON.stringify(data), {
+            status: 400
+        });
+    }
+	return new Response(JSON.stringify(data), {
+		status: 200
+	});
+};
