f3627f88a0a846332fc31212d14f9c3a26a39bcb
Bretislav Dancak <uvqeg@student.kit.edu>
2023-12-06 10:50:08 +0100

    Added start stop component.

---
+++ src/components/StartStop.tsx
@@ -0,0 +1,9 @@
+export const StartStop = () => {
+  return (
+    <label className="swap">
+      <input type="checkbox" />
+      <div className="swap-on">STOP</div>
+      <div className="swap-off">START</div>
+    </label>
+  );
+};
