8790da31760ecd2d3a7c271c334e0712deaea984
davebargel <485304@mail.muni.cz>
2023-12-08 18:50:40 +0100

    added records fetch

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -1,35 +1,60 @@
 import React from "react";
 import { StartStop } from "./StartStop";
+import { getCategoryById } from "@/server/Category/category";
+import { getUserRecords } from "@/server/Record/record";
+import { getServerAuthSession } from "@/server/auth";
+
+interface Record {
+  id: number;
+  records: Array<{
+    deleted: boolean;
+    activityId: number;
+    id: number;
+    endedAt: Date | null;
+    startedAt: Date;
+  }>;
+}
+
+const getTimeSpent = (records: Record[]) => {
+  let time = 0;
+  const now = Date.now();
+
+  records.forEach((record) => {
+    record.records.forEach((record) => {
+      const endedAt = record.endedAt ? new Date(record.endedAt).getTime() : now;
+      const startedAt = new Date(record.startedAt).getTime();
+
+      if (endedAt && startedAt) {
+        time += endedAt - startedAt;
+      }
+    });
+  });
+
+  return Math.floor(time / 1000);
+};
 
 // const ActivityItem = ({ activity }) => {
-const ActivityItem = ({ activity }: { activity: any }) => {
-  const { categoryId, name } = activity;
-
-  // const calculateDuration = () => {
-  //   const durationInMilliseconds = end - start;
-  //   const hours = Math.floor(durationInMilliseconds / (1000 * 60 * 60));
-  //   const minutes = Math.floor(
-  //     (durationInMilliseconds % (1000 * 60 * 60)) / (1000 * 60)
-  //   );
-
-  //   return `${hours}h ${minutes}m`;
-  // };
+const ActivityItem = async ({ activity }: { activity: any }) => {
+  const { categoryId, name, id } = activity;
+  const session = await getServerAuthSession();
+  const category = await getCategoryById({ id: categoryId });
+  if (!session) {
+    return <></>;
+  }
+  const records = await getUserRecords({ userId: session.user.id });
 
   return (
     <div className="bg-gray-800 text-white p-4 mb-4 mx-auto rounded-md shadow-md w-full sm:w-2/5">
       <h3 className="text-xl font-semibold mb-2">{name}</h3>
       <p className="text-gray-400">
-        <strong>Category:</strong>
-        {categoryId}
+        <strong>Category: </strong>
+        {category ? category.name : "Category not found"}
       </p>
-      {/* <p className="text-gray-400">
-        <strong>Total Time:</strong>{" "}
-        {new Date(start).toLocaleTimeString([], {
-          hour: "2-digit",
-          minute: "2-digit",
-        })}
-      </p> */}
-      <StartStop />
+
+      <StartStop
+        activityId={id}
+        timeSpent={getTimeSpent(records.filter((record) => record.id === id))}
+      />
     </div>
   );
 };
