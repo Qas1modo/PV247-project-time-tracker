8d9ec6163fed5fdbd262026f2a1979131ba66952
davebargel <485304@mail.muni.cz>
2023-12-08 18:49:07 +0100

    page hidden when user not logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,20 @@ import Countdown from "../components/Timer";
 import { ActivityContainer } from "../components/ActivitiesContainer";
 import { FilterContainer } from "@/components/FilterContainer";
 import { getActivitiesByUser } from "@/server/Activity/activity";
-import { useSession } from "next-auth/react";
+import { getServerAuthSession } from "@/server/auth";
 
 export default async function Home() {
-  return (
-    <div className="flex-col mx-36">
-      <button></button>
-      <Countdown />
-      <FilterContainer />
-      <ActivityContainer />
-    </div>
-  );
+  const session = await getServerAuthSession();
+  if (session?.user) {
+    return (
+      <div className="flex-col mx-36">
+        <button></button>
+        {/* <Countdown /> */}
+        <FilterContainer />
+        <ActivityContainer />
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 }
