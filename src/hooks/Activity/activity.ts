d9886a2592f2db07ea80bdab83fe7e4f9cd96754
Bretislav Dancak <uvqeg@student.kit.edu>
2023-12-09 12:49:49 +0100

    Added hook to retrieve activities.

--- src/hooks/Activity/activity.ts
+++ src/hooks/Activity/activity.ts
@@ -1,4 +1,21 @@
-import { useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
+
+type Activity = {
+  id: number;
+  createdAt: Date;
+  userId: number;
+  categoryId: number;
+  deleted: boolean;
+  name: string;
+};
+
+export const getActivities = async () => {
+  const response = await fetch("/api/activities", { method: "GET" });
+  return response.json() as Promise<Activity[]>;
+};
+
+export const useGetActivities = () =>
+  useQuery({ queryKey: ["activity"], queryFn: () => getActivities() });
 
 const addActivity = async (data: { name: string; categoryId: number }) => {
   const response = await fetch("/api/activity", {
@@ -9,6 +26,7 @@ const addActivity = async (data: { name: string; categoryId: number }) => {
   const json = await response.json();
   return json;
 };
+export const useAddActivity = () => useMutation({ mutationFn: addActivity });
 
 const getAllActivitiesByCategory = async (data: { categoryId: number }) => {
   const response = await fetch(`/api/activities/${data.categoryId}`, {
@@ -17,15 +35,5 @@ const getAllActivitiesByCategory = async (data: { categoryId: number }) => {
   const json = await response.json();
   return json;
 };
-
-const getAllActivities = async () => {
-  const response = await fetch(`/api/activities`, { method: "GET" });
-  const json = await response.json();
-  return json;
-};
-
-export const useAddActivity = () => useMutation({ mutationFn: addActivity });
 export const useGetAllActivitiesByCategory = () =>
   useMutation({ mutationFn: getAllActivitiesByCategory });
-export const useGetAllActivities = () =>
-  useMutation({ mutationFn: getAllActivities });
