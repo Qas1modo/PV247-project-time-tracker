a76a70962d84988763571ffc30705514a6d6eb0c
davebargel <485304@mail.muni.cz>
2023-12-11 23:16:33 +0100

    added second pie chart with deteails, refactoring

--- src/app/summary/page.tsx
+++ src/app/summary/page.tsx
@@ -1,67 +1,39 @@
 "use client";
 
-import * as React from "react";
 import { PieChart } from "@mui/x-charts/PieChart";
+import {
+  PieItemIdentifier,
+  DefaultizedPieValueType,
+} from "@mui/x-charts/models";
 import { useListRecords } from "@/hooks/Record/record";
 import { useGetActivities } from "@/hooks/Activity/activity";
 import { getTimeSpent } from "@/utils/utils";
 import { useGetCategories } from "@/hooks/Category/category";
 import { colors } from "@/utils/utils";
+import { useState } from "react";
+import { Activity } from "@prisma/client";
 
-// const data = [
-//   { id: 0, value: 10, label: "series A" },
-//   { id: 1, value: 15, label: "series B" },
-//   { id: 2, value: 20, label: "series C" },
-// ];
+type PieChartRecord = {
+  id: number;
+  value: number;
+  label: string;
+  color?: string;
+};
 
-const SummaryPage = () => {
-  const { data: records, isLoading: recordsLoading } = useListRecords();
-  const { data: activities, isLoading: activitiesLoading } = useGetActivities();
-
-  const { data: categories, isLoading: categoriesLoading } = useGetCategories();
-  if (recordsLoading || activitiesLoading || categoriesLoading) {
-    return <>Loading...</>;
-  }
-  let totalTimeSpent = 0;
-
-  let timePerActivity: Record<number, any> = {};
-
-  if (records) {
-    records.forEach((record) => {
-      const timeSpent = getTimeSpent(record.records);
-      if (!timePerActivity[record.id]) {
-        timePerActivity[record.id] = timeSpent;
-      } else {
-        timePerActivity[record.id] += timeSpent;
-      }
-      totalTimeSpent += timeSpent;
-    });
-  }
-
-  let data: { id: number; value: number; label: string; color: string }[] = [];
-
-  categories?.forEach((category) => {
-    let timeTotal = 0;
-    for (const [activityId, timeSpent] of Object.entries(timePerActivity)) {
-      if (
-        activities?.find((activity) => activity.id === Number(activityId))
-          ?.categoryId === category.id
-      ) {
-        timeTotal += timeSpent;
-      }
-    }
-    const percentage = (timeTotal / totalTimeSpent) * 100;
-    if (timeTotal > 0)
-      data.push({
-        id: category.id,
-        value: percentage,
-        label: category.name,
-        color: colors[category.id],
-      });
-  });
-
+const PieChartComponent = ({
+  data,
+  handleClick,
+}: {
+  data: PieChartRecord[];
+  handleClick?: (
+    _event: React.MouseEvent<SVGPathElement, MouseEvent>,
+    _itemIdentifier: PieItemIdentifier,
+    item: DefaultizedPieValueType
+  ) => void;
+}) => {
   return (
     <PieChart
+      onClick={handleClick}
       series={[
         {
           data,
@@ -85,4 +57,86 @@ const SummaryPage = () => {
   );
 };
 
+const SummaryPage = () => {
+  const [selectedCategory, setSelectedCategory] = useState<number | null>(null);
+  const { data: records, isLoading: recordsLoading } = useListRecords();
+  const { data: activities, isLoading: activitiesLoading } = useGetActivities();
+
+  const { data: categories, isLoading: categoriesLoading } = useGetCategories();
+  if (recordsLoading || activitiesLoading || categoriesLoading) {
+    return <>Loading...</>;
+  }
+  let totalTimeSpent = 0;
+
+  let timePerActivity: Record<number, any> = {};
+
+  if (records) {
+    records.forEach((record) => {
+      const timeSpent = getTimeSpent(record.records);
+      timePerActivity[record.id] =
+        (timePerActivity[record.id] || 0) + timeSpent;
+      totalTimeSpent += timeSpent;
+    });
+  }
+
+  let data: PieChartRecord[] = [];
+  let timePerActivityPerCategory: Record<number, any> = {};
+
+  categories?.forEach((category) => {
+    const activitiesInCategory = activities?.filter(
+      (activity) => activity.categoryId === category.id
+    );
+
+    const activitiesWithTimeSpent = activitiesInCategory?.map((activity) => {
+      const timeSpent = timePerActivity[activity.id] || 0;
+      return { activity, timeSpent };
+    });
+
+    const timeTotal = activitiesWithTimeSpent?.reduce(
+      (total, { timeSpent }) => total + timeSpent,
+      0
+    );
+
+    if (timeTotal && activitiesWithTimeSpent?.length) {
+      data.push({
+        id: Number(category.id),
+        value: (timeTotal / totalTimeSpent) * 100,
+        label: category.name,
+        color: colors[category.id],
+      });
+
+      timePerActivityPerCategory[category.id] = activitiesWithTimeSpent;
+    }
+  });
+
+  const handleClick = (
+    _event: React.MouseEvent<SVGPathElement, MouseEvent>,
+    _itemIdentifier: PieItemIdentifier,
+    item: DefaultizedPieValueType
+  ) => {
+    setSelectedCategory(Number(item.id));
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <PieChartComponent data={data} handleClick={handleClick} />
+      <div className="mt-10"></div>
+      {selectedCategory ? (
+        <PieChartComponent
+          data={timePerActivityPerCategory[selectedCategory].map(
+            (item: { activity: Activity; timeSpent: number }) => ({
+              id: item.activity.id,
+              value: item.timeSpent,
+              label: item.activity.name,
+            })
+          )}
+        />
+      ) : (
+        <p className="font-xs text-slate-500 ">
+          Click on the chart above to see details
+        </p>
+      )}
+    </div>
+  );
+};
 export default SummaryPage;
