751c63199f397d038dee33ed0f0b0286d291d4cd
Radomír Mann <493346@mail.muni.cz>
2023-12-12 21:44:18 +0100

    Vercel build

--- vercel-build.js
+++ vercel-build.js
@@ -2,7 +2,10 @@ const { execSync } = require('child_process');
 
 try {
   // Run Prisma commands during build
+  execSync('npm i');
   execSync('npx prisma generate');
+  execSync('npx prisma migrate reset');
+  execSync('npm run dev');
 } catch (error) {
   console.error('Error running Prisma commands during build:', error);
   process.exit(1);
